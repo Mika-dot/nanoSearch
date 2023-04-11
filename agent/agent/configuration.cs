4352a3a94e92c1fcb6a5cbd97ffcb16d5e786480
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2023-04-11 22:33:47 +0300

    Add files via upload
    Made the main classes of the agent, no gradient and circling added

---
+++ agent/agent/configuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agentConfiguration
+{
+    static class configuration
+    {
+        static public int AltitudeMin { get; set; } = -10;// выстора критерий
+        static public int AltitudeMax { get; set; } = 10;
+
+        static public float CornerMin { get; set; } = -45;// угол критерий
+        static public float CornerMax { get; set; } = 45;
+
+        static public float lengthMax { get; set; } = 5;// максимальный длина
+
+        static public float cornerMin { get; set; } = 45;// минимальный угол
+
+        static public Point end { get; set; } = new Point(80, 50);
+
+        static public SquareGrid? grid { get; set; } = new SquareGrid(Map.GetLength(0), Map.GetLength(0));
+
+        static public int[,] Map;
+    }
+
+}
