ac8bc18966534cfdbb3de6ed4cefb6aef58b3aed
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2023-04-21 00:37:23 +0300

    Add files via upload
    updated

--- AgentSmith/Agent.cs
+++ AgentSmith/Agent.cs
@@ -14,6 +14,24 @@ namespace AgentSmith
             Configuration.Map = Maps;
             Configuration.End = end;
         }
+        public enum CriterionName : int
+        {
+            AStar,
+            Height,
+            Corner,
+            Length,
+            AngleOfRotation,
+        }
+
+        public enum Border : int
+        {
+            AltitudeMin,
+            AltitudeMax,
+            CornerHeightsMin,
+            CornerHeightsMax,
+            LengthMax,
+            CornerMin,
+        }
 
         public (Point, bool[])? AgentActions(Point position, Point historyPosition)
         {
@@ -60,6 +78,85 @@ namespace AgentSmith
             Point offset = new Point(Cof.DIRS[Best].X, Cof.DIRS[Best].Y);
 
             return (new Point(position.X + offset.X, position.Y + offset.Y), Flags);
+        }
+
+        public Agent Criterion(CriterionName name, float coefficient = 1)
+        {
+            switch (name)
+            {
+                case CriterionName.AStar:
+                    Coefficient.AStarSearch = coefficient;
+                    NonlinearFunction(CriterionName.AStar);
+                    break;
+                case CriterionName.Height:
+                    Coefficient.Height = coefficient;
+                    NonlinearFunction(CriterionName.Height);
+                    break;
+                case CriterionName.Corner:
+                    Coefficient.Corner = coefficient;
+                    NonlinearFunction(CriterionName.Corner);
+                    break;
+                case CriterionName.Length:
+                    Coefficient.Length = coefficient;
+                    NonlinearFunction(CriterionName.Length);
+                    break;
+                case CriterionName.AngleOfRotation:
+                    Coefficient.AngleOfRotation = coefficient;
+                    NonlinearFunction(CriterionName.AngleOfRotation);
+                    break;
+            }
+            return this;
+        }
+
+        public Agent NonlinearFunction(CriterionName name, float[,] points = null)
+        {
+            points ??= new float[,] { { 0, 0 }, { 1, 1 }, { 2, 2 }, { 3, 3 } };
+
+            switch (name)
+            {
+                case CriterionName.AStar:
+                    calculationFormula.AStarSearch = points;
+                    break;
+                case CriterionName.Height:
+                    calculationFormula.Height = points;
+                    break;
+                case CriterionName.Corner:
+                    calculationFormula.Corner = points;
+                    break;
+                case CriterionName.Length:
+                    calculationFormula.Length = points;
+                    break;
+                case CriterionName.AngleOfRotation:
+                    calculationFormula.AngleOfRotation = points;
+                    break;
+            }
+            return this;
+        }
+
+        public Agent BorderValuesFlags(Border name, float value)
+        {
+            switch (name)
+            {
+                case Border.AltitudeMin:
+                    Configuration.AltitudeMin = (int)value;
+                    break;
+                case Border.AltitudeMax:
+                    Configuration.AltitudeMax = (int)value;
+                    break;
+                case Border.CornerHeightsMin:
+                    Configuration.CornerHeightsMin = value;
+                    break;
+                case Border.CornerHeightsMax:
+                    Configuration.CornerHeightsMax = value;
+                    break;
+                case Border.LengthMax:
+                    Configuration.LengthMax = value;
+                    break;
+                case Border.CornerMin:
+                    Configuration.CornerMin = value;
+                    break;
+            }
+            return this;
         }
 
     }
