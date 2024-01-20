22ae55d3f17fff27d034ae4cac436368fb36f1b0
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2024-01-20 04:52:06 +0300

    Update PriorityQueue.cs

--- A-Star/PriorityQueue.cs
+++ A-Star/PriorityQueue.cs
@@ -2,15 +2,6 @@
 {
     class PriorityQueue<T>
     {
-        // В этом примере я использую несортированный массив, но в идеале
-        // это должна быть двоичная куча. Существует открытый запрос на добавление
-        // двоичной кучи к стандартной библиотеке C#: https://github.com/dotnet/corefx/issues/574
-        //
-        // Но пока её там нет, можно использовать класс двоичной кучи:
-        // * https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp
-        // * http://visualstudiomagazine.com/articles/2012/11/01/priority-queues-with-c.aspx
-        // * http://xfleury.github.io/graphsearch.html
-        // * http://stackoverflow.com/questions/102398/priority-queue-in-net
 
         private List<(T, double)> elements = new List<(T, double)>();
 
